766c3cd7e6679645d7d66799ba9806d5a435ef9e
Parul Parbhakar <93875141+NUTTER-11@users.noreply.github.com>
2023-09-07 11:48:34 +0530

    Create contactdata.js

---
+++ contactdata.js
@@ -0,0 +1,44 @@
+/** @format */
+
+// export const icons = [
+//   "fab fa-facebook-f",
+//   "fab fa-twitter",
+//   "fab fa-pinterest",
+//   "fab fa-instagram",
+//   "fab fa-whatsapp",
+// ];
+
+export const contactInfo = [
+  {
+    title: "ADDRESS:",
+    subtitle: "Restaurant",
+    details: "96, Mendip Cresent,ESSEX",
+  },
+  {
+    title: "RESERVATION:",
+    subtitle: "+ 441 284 2345",
+    details: "lovefood96@gmail.com",
+  },
+  {
+    title: "OPEN HOURS:",
+    subtitle: "Monday-Friday 10AM-11AM",
+    details: "Saturday-Sunday 12AM-1PM",
+  },
+];
+export const contactInput = [
+  {
+    label: "name",
+    title: "Name",
+    type: "text",
+  },
+  {
+    label: "email",
+    title: "Email",
+    type: "email",
+  },
+  {
+    label: "subject",
+    title: "Subject",
+    type: "text",
+  },
+];
